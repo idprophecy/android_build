1b8c9667c4c67f6ec413262368d78d70bd81d14b
Martin Otto <paraboloid234@gmail.com>
2018-03-03 08:07:19 +0100

    switch to the upstream google clang-4630689

--- core/clang/versions.mk
+++ core/clang/versions.mk
@@ -1,5 +1,5 @@
 ## Clang/LLVM release versions.
 
 LLVM_RELEASE_VERSION := 6.0.1
-LLVM_PREBUILTS_VERSION ?= clang-4579689
+LLVM_PREBUILTS_VERSION ?= clang-4630689
 LLVM_PREBUILTS_BASE ?= prebuilts/clang/host
