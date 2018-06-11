7e9a9c748cafb1f998f3b26f5522482008a83d70
Martin Otto <paraboloid234@gmail.com>
2018-06-11 21:05:41 +0200

    Revert "use ThinLTO"
    This reverts commit 9158f8dc36641f5e5568ec7138573d6f95e147a4.

--- core/binary.mk
+++ core/binary.mk
@@ -373,7 +373,7 @@ my_target_global_ldflags := $($(LOCAL_2ND_ARCH_VAR_PREFIX)CLANG_TARGET_GLOBAL_LD
 
         ifeq ($(LOCAL_SDCLANG_LTO), true)
             ifneq ($(LOCAL_MODULE_CLASS), STATIC_LIBRARIES)
-                SDCLANG_PRECONFIGURED_FLAGS += -fuse-ld=qcld -flto=thin
+                SDCLANG_PRECONFIGURED_FLAGS += -fuse-ld=qcld -flto
             endif
         endif
 
